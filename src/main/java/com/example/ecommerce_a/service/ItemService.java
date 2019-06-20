d76ae1a36e2b8c2a4cdbb0d18e1f9477ee939635
sho.ikehara <sho.ikehara@rakus.co.jp>
2019-06-20 11:22:08 +0900

    新規にItemServiceクラスを作成。findAll,findByName,load機能を追加。

---
+++ src/main/java/com/example/ecommerce_a/service/ItemService.java
@@ -0,0 +1,29 @@
+package com.example.ecommerce_a.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.ecommerce_a.domain.Item;
+import com.example.ecommerce_a.repository.ItemRepository;
+
+@Service
+@Transactional
+public class ItemService {
+	@Autowired
+	private ItemRepository itemRepository;
+	
+	public List<Item> findAll(){
+		return itemRepository.findAll();
+	}
+	
+	public List<Item> findByName(String name){
+		return itemRepository.findByName(name);
+	}
+	
+	public Item load(Integer id) {
+		return itemRepository.load(id);
+	}
+}
