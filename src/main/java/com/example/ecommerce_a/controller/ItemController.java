2e7c820466fa4748e25c4cddd70308e8f3df0b4d
sho.ikehara <sho.ikehara@rakus.co.jp>
2019-06-20 11:32:44 +0900

    新規にItemControllerを作成。showListメソッドのみ作成済。

---
+++ src/main/java/com/example/ecommerce_a/controller/ItemController.java
@@ -0,0 +1,25 @@
+package com.example.ecommerce_a.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.ecommerce_a.domain.Item;
+import com.example.ecommerce_a.service.ItemService;
+
+@Controller
+@RequestMapping("/item")
+public class ItemController {
+	@Autowired
+	ItemService itemService;
+	
+	@RequestMapping("/showList")
+	public String showList(Model model) {
+		List<Item> itemList = itemService.findAll();
+		model.addAttribute("itemList",itemList);
+		return "item_list";
+	}
+}
