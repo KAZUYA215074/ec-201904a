92027b96eae1e51c23dfceb23555ba40bfd15b5e
MakotoKobayashi0 <b1500578@planet.kanazawa-it.ac.jp>
2019-06-21 08:57:25 +0900

    商品詳細画面で動的に金額が変わるようにした

---
+++ src/main/resources/static/js/autoCalcPrice.js
@@ -0,0 +1,36 @@
+var sizeMappingPrice = {
+    "M": 1380, 
+    "L": 2380
+};
+
+var toppingMappingPrice = {
+    "M": 200, 
+    "L": 300
+};
+
+let calcSumPrice = () => {
+    var size = $("input[name=responsibleCompany]:checked").val()
+    var selectedToppingNum = $("input[class=toppingCheckbox]:checked").length;
+    var quantitiy = $(".form-control").val()
+    var price = (toppingMappingPrice[size]
+		        * selectedToppingNum
+		        + sizeMappingPrice[size])
+		        * quantitiy;
+    $("#total-price").text("この商品金額：" + price + "円(税抜)");
+}
+
+$(function () {
+    calcSumPrice()
+
+    $("input[name=responsibleCompany]:radio").click(function () {
+        calcSumPrice()
+    })
+
+    $(".toppingCheckbox").change(function () {
+        calcSumPrice()
+    })
+
+    $(".form-control").change(function () {
+        calcSumPrice()
+    })
+});
