185d6c328d6ce60fdf8cee77b98a16866de38c8e
sho.ikehara <sho.ikehara@rakus.co.jp>
2019-06-21 11:07:53 +0900

    Sort機能とページング機能を一緒にできるようにした。

---
+++ src/main/java/com/example/ecommerce_a/form/SortForm.java
@@ -0,0 +1,13 @@
+package com.example.ecommerce_a.form;
+
+public class SortForm {
+	private String sortName;
+
+	public String getSortName() {
+		return sortName;
+	}
+
+	public void setSortName(String sortName) {
+		this.sortName = sortName;
+	}
+}
