2659f43ae4b0019c9a3a4729a5ab5db75937b2d2
takatoshi.donga <takatoshi.donga@rakus.co.jp>
2019-06-21 14:09:15 +0900

    郵便番号と電話番号の変換メソッドを追加.

---
+++ src/main/java/com/example/ecommerce_a/utils/ConvertUtils.java
@@ -0,0 +1,47 @@
+package com.example.ecommerce_a.utils;
+
+import com.google.i18n.phonenumbers.NumberParseException;
+import com.google.i18n.phonenumbers.PhoneNumberUtil;
+import com.google.i18n.phonenumbers.Phonenumber;
+
+/**
+ * 変換系のメソッド集.
+ * 
+ * @author taka
+ *
+ */
+public class ConvertUtils {
+
+	/**
+	 * 郵便番号からハイフンを削除する.
+	 * 
+	 * @return zipCode
+	 */
+	public static String getDelHyphenZipCode(String zipCode) {
+		StringBuffer hyphenZipCode = new StringBuffer(zipCode);
+		int hyphenIndex = hyphenZipCode.indexOf("-");
+		if (hyphenIndex == -1) {
+			return zipCode;
+		} else {
+			hyphenZipCode.deleteCharAt(3);
+			return hyphenZipCode.toString();
+		}
+	}
+
+	/**
+	 * 電話番号にハイフンをつける.
+	 * 
+	 * @param telephone 電話番号
+	 * @return ハイフンをつけた電話番号
+	 */
+	public static String getHypehnTelephone(String telephone) {
+		final PhoneNumberUtil PHONE_NUMBER_UTIL = PhoneNumberUtil.getInstance();
+		try {
+			Phonenumber.PhoneNumber phoneNumber = PHONE_NUMBER_UTIL.parse(telephone, "JP");
+			return PHONE_NUMBER_UTIL.format(phoneNumber, PhoneNumberUtil.PhoneNumberFormat.NATIONAL);
+		} catch (NumberParseException e) {
+			return telephone;
+		}
+	}
+
+}
