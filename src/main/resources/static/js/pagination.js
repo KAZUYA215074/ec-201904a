39e7e013c717bb40d0187414f033f8334ae22b72
sho.ikehara <sho.ikehara@rakus.co.jp>
2019-06-26 12:43:26 +0900

    注文状況のviewを見やすく変更。ページネーションを追加。

--- src/main/resources/static/js/pagination.js
+++ src/main/resources/static/js/pagination.js
@@ -8,3 +8,14 @@ $(function() {
 		firstEndPageBtnMode            : false,
     });
 });
+
+$(function() {
+    $('.orderItems').pagination({
+        itemElement : '> .orderItem',// アイテムの要素
+        displayItemCount  : 3,
+        paginationClassName            : 'pagination',
+		paginationInnerClassName       : 'pagination',
+		prevNextPageBtnMode            : false,
+		firstEndPageBtnMode            : false,
+    });
+});
